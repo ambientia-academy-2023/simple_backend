592d3c34ef871bd7e031a875bf6d04001e846ae8
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-01-20 13:32:38 +0200

    esimerkki valmis

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+const { request, response } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/example1',
     function(request,response){
@@ -9,6 +13,12 @@ app.get('/example1',
     }
 );
 
+app.use(
+    function(request,response,next){
+        console.log("Olen Middleware1");
+        next();
+});
+
 app.get('/example2',
     function(request, response){
         console.log("endpoint example2");
@@ -16,8 +26,36 @@ app.get('/example2',
 
     }
 );
+app.use(
+    function(request,response,next){
+        console.log("Olen Middleware2");
+        next();
+});
+app.get('/example3/:fn',
+    function(request,response){
+        let fname=request.params.fn;
+        response.json("Terve "+fname);
+        console.log("Terve "+fname);
+    }
+);
 
+app.get('/example4/:fn?',
+    function(request,response){
+        if(request.params.fn){
+            response.json("Nimi = "+request.params.fn);
+        }
+        else{
+            response.json("Et antanut nimeä");
+        }
+    }
+);
 
+app.post('/example1',
+    function(request, response){
+        response.json(request.body.fname);
+        console.log(request.body);
+    }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
